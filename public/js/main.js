230db76a1204ba859d5e477f95b94a0107416651
DESKTOP-1SDMCHM\Margarita <match14@yandex.ru>
2019-09-16 01:51:46 +0300

    Часть адаптива главной

--- public/js/main.js
+++ public/js/main.js
@@ -77,7 +77,15 @@ $(document).ready(function () {
 		$('.js-partner').slick({
 			infinite: true,
 			slidesToShow: 5,
-			slidesToScroll: 1
+			slidesToScroll: 1,
+			responsive: [
+				{
+					breakpoint: 991,
+					settings: {
+						slidesToShow: 3,
+					}
+				},
+			]
 		});
 	}
 
